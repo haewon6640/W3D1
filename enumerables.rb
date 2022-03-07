ad413d6fa12a05004ed218a5e3f4b9c265a7111f
Haewon6640 <haewon6640@gmail.com>
2022-03-07 17:58:57 -0500

    my_each

--- enumerables.rb
+++ enumerables.rb
@@ -14,7 +14,43 @@ class Array
     self.my_each  { |val| newArr << val if prc.call(val) }
     newArr
   end
+  
+  def my_reject(&prc)
+    newArr = []
+    self.my_each  { |val| newArr << val if !prc.call(val) }
+    newArr
+  end
+
+  def my_any?(&prc)
+    self.my_each  { |val| return true if prc.call(val) }
+    false
+  end
+
+  def my_all?(&prc)
+    self.my_each  { |val| return false if !prc.call(val) }
+    true
+  end
+#   [[1] 2 3 [4,5,6]]
+#   [1 [2 3] [4,5,6]]
+  def my_flatten
+    return [self[0]] if !self[0].is_a?(Array)
+    return self[0] if self.length == 0
+    p "#{self[0]}  #{self[1..-1]}"
+    self[0].my_flatten + self[1..-1].my_flatten
+  end
 end
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
 =begin
 p a = [1, 2, 3]
